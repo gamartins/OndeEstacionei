68a628d173e4d9798d86411a43af779589e2b8bb
Gabriel Angelo <gabriel.angelo91@gmail.com>
2017-02-23 22:58:35 -0300

    Showing latitude and longitude dynamically

---
+++ app/src/main/java/com/example/gabriel/ondeestacionei/LocationProvider.java
@@ -0,0 +1,107 @@
+package com.example.gabriel.ondeestacionei;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+
+/**
+ * Created by Gabriel on 23/02/2017.
+ */
+
+public class LocationProvider implements
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        LocationListener {
+
+    public interface LocationCallback {
+        void handleNewLocation(Location location);
+    }
+
+    public static final String TAG = LocationProvider.class.getSimpleName();
+    public static final int REQUEST_PERMISSION_ACCESS_FINE_LOCATION = 1;
+
+    private LocationCallback locationCallback;
+    private GoogleApiClient googleApiClient;
+    private LocationRequest locationRequest;
+    private Context context;
+
+    public LocationProvider(Context context, LocationCallback locationCallback) {
+        this.locationCallback = locationCallback;
+        this.context = context;
+
+        googleApiClient = new GoogleApiClient.Builder(context)
+                .addApi(LocationServices.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+
+        // Creating the LocationRequest objetct
+        locationRequest = LocationRequest.create()
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                .setInterval(3 * 1000)   // 3 seconds
+                .setFastestInterval(1 * 1000);  // 1 second
+
+    }
+
+    public void connect() {
+        googleApiClient.connect();
+    }
+
+    public void disconnect() {
+        if (googleApiClient.isConnected()) {
+            LocationServices.FusedLocationApi.removeLocationUpdates(googleApiClient, this);
+            googleApiClient.disconnect();
+        }
+    }
+
+    private void getPermission(){
+        ActivityCompat.requestPermissions((Activity) context,
+                new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                REQUEST_PERMISSION_ACCESS_FINE_LOCATION);
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        Log.d(TAG, "Location services connected");
+
+        // Checking for permission
+        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            getPermission();
+        }
+
+        Location location = LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
+        LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient, locationRequest, this);
+
+        locationCallback.handleNewLocation(location);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        connect();
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.e(TAG, "ConnectionFailed");
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        locationCallback.handleNewLocation(location);
+    }
+}
--- app/src/main/java/com/example/gabriel/ondeestacionei/MainActivity.java
+++ app/src/main/java/com/example/gabriel/ondeestacionei/MainActivity.java
@@ -1,13 +1,41 @@
 package com.example.gabriel.ondeestacionei;
 
+import android.location.Location;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LocationProvider.LocationCallback {
+    private LocationProvider locationProvider;
+    private TextView latitudeTextView, longitudeTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        latitudeTextView = (TextView) findViewById(R.id.latitudeTextView);
+        longitudeTextView = (TextView) findViewById(R.id.longitudadeTextView);
+
+        locationProvider = new LocationProvider(this, this);
+    }
+
+    @Override
+    protected void onStart() {
+        locationProvider.connect();
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        locationProvider.disconnect();
+        super.onStop();
+    }
+
+    @Override
+    public void handleNewLocation(Location location) {
+        longitudeTextView.setText(String.valueOf(location.getLongitude()));
+        latitudeTextView.setText(String.valueOf(location.getLatitude()));
     }
 }
